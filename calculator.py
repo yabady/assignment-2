65bb0a7df59361bb13e62e75d6eeeb845dee4817
Yasser Abady <yasser.abady@ryerson.ca>
2021-04-10 12:54:24 -0400

    initial

---
+++ calculator.py
@@ -0,0 +1,24 @@
+
+class Calculator:
+
+  def __init__(self):
+    pass
+
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+  def subtract(self, a, b):
+    return a - b
+
+  def divide(self, a, b):
+    if b != 0:
+      return a/b
+
+def add_all(int_list):
+    sum = 0
+    for n in int_list:
+        sum = sum + n
+    return sum
---
+++ unit_tests.py
@@ -0,0 +1,33 @@
+import unittest
+from calculator import Calculator, add_all
+
+class TestCalculator(unittest.TestCase):
+
+    # Test Calculator
+
+    def setUp(self):
+        self.calculator = Calculator()
+  
+    def test_add(self):
+        self.assertEqual(self.calculator.add(5,5), 10)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(6,5), 1)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(5,3), 15)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(6,2), 3)
+
+    # Test divide when denominator is 0
+    def test_divide_with_zero(self):
+        self.assertEqual(self.calculator.divide(6,0), None)
+
+
+class TestAddAll(unittest.TestCase):
+    def test_add_all(self):
+        self.assertEqual(add_all([2,2,2]), 6)
+
+if __name__ == "__main__":
+    unittest.main()
